bd5e384f85a2d8eeef3471c70abaf05e8a5ec0d4
Vargas991 <eddersonvargas91@gmail.com>
2026-04-29 18:49:54 -0400

    change conf vercer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL ?? "https://industriasmmca.onrender.com",
   output: "server",
-  security: {
-    checkOrigin: false,
-  },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   integrations: [sitemap()],
   vite: {
     resolve: {
