82764ba8b7b08e285199a65c3accf689cf1e57d4
Vargas991 <eddersonvargas91@gmail.com>
2026-05-19 16:07:18 -0400

    config sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL ?? "https://industriasmmca.onrender.com",
+  site: process.env.PUBLIC_SITE_URL ?? "https://industriasmmca.com",
   output: "server",
   security: {
     checkOrigin: false,
@@ -12,7 +12,11 @@ export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (url) => !url.includes("/admin/"),
+    }),
+  ],
   vite: {
     resolve: {
       alias: {
